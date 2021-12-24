9d2396c187c4da85bc3db85bfceae13a260776a0
Tseng Shih Yang <iamdog43gmail.com>
2021-12-24 15:00:18 +0800

    完成校園映像圖片輪播

--- front/main.php
+++ front/main.php
@@ -3,6 +3,7 @@
     <?php include "marquee.php";?>
     <div style="height:32px; display:block;"></div>
     <!--正中央-->
+
     <div style="width:100%; padding:2px; height:290px;">
         <div id="mwww" loop="true" style="width:100%; height:100%;">
             <div style="width:99%; height:100%; position:relative;" class="cent">沒有資料</div>
@@ -10,21 +11,18 @@
     </div>
     <script>
     var lin = new Array();
-  
- <?php 
-$mvs=$Mvim->all(['sh'=>1]);
-foreach($mvs as $mv){
-?>
-lin.push('<?="img/{$mv['img']}";?>')
+    <?php
+        //撈出所有要顯示的動畫圖片並放入JS陣列中
+        $mvs=$Mvim->all(['sh'=>1]);
+        foreach($mvs as $mv){
+        ?>
+            lin.push('<?="img/{$mv['img']}";?>')
+        <?php    
+        }
 
-<?php
-}
-
-?>
-    // lin[0]='  ...' ;   
-    // lin[1]='  ...' ;   
+    ?>
     var now = 0;
-    ww()
+    ww()   //先執行一次
     if (lin.length > 1) {
         setInterval("ww()", 3000);
         now = 1;
@@ -40,13 +38,29 @@ lin.push('<?="img/{$mv['img']}";?>')
     <div
         style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
         <span class="t botli">最新消息區
+            <?php 
+                if($News->math('count','*',['sh'=>1])>5){
+                    echo "<a href='?do=news' style='float:right'>More...</a>";
+                }
+            ?>
         </span>
         <ul class="ssaa" style="list-style-type:decimal;">
+        <?php
+            $news=$News->all(['sh'=>1]," LIMIT 5");
+            foreach($news as $n){
+                echo "<li>";
+                echo mb_substr($n['text'],0,20);
+                echo "<div class='all' style='display:none'>{$n['text']}</div>";
+                echo "</li>";
+            }
+
+        ?>
         </ul>
         <div id="altt"
             style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
         </div>
         <script>
+
         $(".ssaa li").hover(
             function() {
                 $("#altt").html("<pre>" + $(this).children(".all").html() + "</pre>")
